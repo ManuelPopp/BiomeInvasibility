9d9eaa053acb4f4090bc94c17b5a1cd7548042c8
Manuel Richard Popp <manuel.popp@wsl.ch>
2024-12-08 00:14:33 +0100

    Fixed imports

--- rsc/download_observations.R
+++ rsc/download_observations.R
@@ -17,17 +17,49 @@
 #>----------------------------------------------------------------------------<|
 #> Install/load packages
 rm(list = ls())
+import <- function(...) {
+  #' Import R packages. Install them if necessary.
+  #' 
+  #' @param ... any argument that can be passed to install.packages.
+  #' @details The function installs only packages that are missing. Packages
+  #' are loaded.
+  #' @examples
+  #' # Load packages
+  #' import("dplyr", "MASS", "terra", dependencies = TRUE)
+  #' 
+  #' @seealso \code{\link[base]{install.packages}}
+  #' @export
+  args <- list(...)
+  packages = args[names(args) == ""]
+  kwargs = args[names(args) != ""]
+  
+  for (package in packages) {
+    if (!require(package, character.only = TRUE)) {
+      do.call(install.packages, c(list(package), kwargs))
+    }
+    require(package, character.only = TRUE)
+  }
+}
+
+import(
+  "dplyr", "devtools",
+  dependencies = TRUE
+)
 
 if (!require("GBIFhandleR", character.only = TRUE)) {
   devtools::install_github("https://github.com/ManuelPopp/GBIFhandleR")
 }
 
 require("GBIFhandleR")
-require("dplyr")
 
 #>----------------------------------------------------------------------------<|
 #> Settings
-dir_main <- "C:/Users/poppman/switchdrive/PhD/prj/bir/git/BiomeInvasibility"
+if (Sys.info()["sysname"] == "Windows") {
+  dir_main <- "C:/Users/poppman/switchdrive/PhD/prj/bir/git/BiomeInvasibility"
+} else {
+  dir_main <- "/lud11/poppman"
+}
+
 dir_dat <- file.path(dir_main, "dat")
 dir_obs <- file.path(dir_dat, "obs")
 
